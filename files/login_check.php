257063ddbc9034a5dfbd33c69a7b4f43603a641f
Josep-merce <jdominguez@lamerce.com>
2024-10-25 13:06:44 +0200

    Update login_check.php

--- files/login_check.php
+++ files/login_check.php
@@ -1,6 +1,8 @@
 <?php
 include "config.php";
 
+echo "Hola";
+
 
 	if (($_POST['usuari'] === USER) && (password_verify($_POST['pass'],USER_PASS))||
                 ($_POST['usuari'] === USER2) && (password_verify($_POST['pass'],USER_PASS2))){
